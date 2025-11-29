d8f06f61a935ff3be9f6674f2b25ff99a76fbfde
47seconds <vaibhavlakshkar.12345@gmail.com>
2025-11-30 00:11:05 +0530

    minor changes'

--- src/archiver.c
+++ src/archiver.c
@@ -1,11 +1,14 @@
 #include "../include/archiver.h"
 #include "../include/files.h"
 #include "../util/common.h"
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 char *archive_files(char **argv) {
-  if (check_if_exist(argv[2]) == -1) {
+  short file_type = 2;
+  file_type = check_if_exist(argv[2]);
+  if (file_type == -1) {
     fprintf(stderr, "ERROR: %s not exist.\n", argv[2]);
     exit(EXIT_FAILURE);
   }
